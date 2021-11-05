436196c56873b360cb553163a519856b7ca6fe04
MaxRudim <max.hermsdorff@gmail.com>
2021-11-05 02:43:25 -0300

    (commit: 11) - front-end: adiciona card para ser renderizado em Home

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const Controller = require('./src/controllers/todo');
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
 const app = express();
 app.use(bodyParser.json());
-const port = 3000;
+const port = 3001;
+
+app.use(cors(corsOptions));
 
 app.post('/task', Controller.createController);
 app.get('/task', Controller.getAllController);
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,13 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tasks, setTask] = useState();
+
   useEffect(() => {
+    setIsLoading(true)
     fetch('http://localhost:3001')
     .then(response => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      setTask(data);
+      setIsLoading(false)
+    })
   }, []);
   return (
-    <h1>Lista de tarefas</h1>
+    <div>
+      <h1>Lista de tarefas Ebytr.</h1>
+
+      {isLoading ? <p>Carregando...</p>
+        : ( 
+          <div>
+            {tasks.map(task => (
+              <div className="tasks-container" key={task.id}>
+                <h2 className="tasks-description">{task.description}</h2>
+                <h3 className="tasks-deadline">{task.deadline}</h3>
+                <p className="tasks-assignee">{task.assignee}</p>
+                <p className="tasks-assignedby">{task.assignedBy}</p>
+              </div>
+            ))}
+          </div>
+      )}
+    </div>    
   );
 }
 
